acff1e735947b94f6782b3ec3f211c441e458e02
Febryani Sri Waranti <febryaniswr@gmail.com>
2021-09-27 15:59:00 +0700

    Update AccountRepository.cs

--- NETCore/Repository/Data/AccountRepository.cs
+++ NETCore/Repository/Data/AccountRepository.cs
@@ -154,7 +154,7 @@ namespace NETCore.Repository.Data
                 smtp.Host = "smtp.gmail.com"; //for gmail host  
                 smtp.EnableSsl = true;
                 smtp.UseDefaultCredentials = false;
-                smtp.Credentials = new NetworkCredential("febyaniwaranti@gmail.com", "febryanisriwaranti12");
+                smtp.Credentials = new NetworkCredential("febyaniwaranti@gmail.com", "***********");
                 smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
                 smtp.Send(message);
             }
